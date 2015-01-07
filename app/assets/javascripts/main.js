106d4f7941ec9334d2456699b890331cd2c3a884
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-06 16:29:41 -0800

    Added question choices with radio buttons

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,6 @@
 var menuTemplate = _.template(
   "<% _.forEach(content, function(e) { %>" +
-    "<li><%= e.title %></li>" +
+    "<li class=\"quiz\" data-id=\"<%= e.id %>\"><%= e.title %></li>" +
   "<% }) %>"
 );
 
@@ -10,3 +10,29 @@ $(function() {
     $('#menu').append(compiled);
   });
 });
+
+var questionsTemplate = _.template(
+  "<ol>" +
+    "<% _.forEach(content, function(data) { %>" +
+      "<li><%= data.question %> </li>" +
+        "<ul class=\"choices-block\">" +
+          "<% var options = data.choices.split(';') %>" +
+          "<% options.forEach(function (o) { %>" +
+            "<li><input type='radio' name='question-<%= data.id %>' value='<%= o %>'><%= o %></li>" +
+          "<% }) %>" +
+        "</ul>" +
+    "<% }) %>" +
+  "</ol>"
+);
+
+$(document).on('click', '.quiz', function() {
+  $('#quizzes-display').empty(); //clear the question block 
+  var quizID = $(this).data("id");
+  var url = "/quizzes/" + quizID + "/questions";
+  $.get(url, function(questionData) {
+    console.log(questionData);
+    var $el = questionsTemplate({content: questionData});
+    $('#quizzes-display').append($el);
+  });
+});
+
