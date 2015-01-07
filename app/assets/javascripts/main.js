f3ffbc5dbce773fb4d1c80e691b13300da38634c
Mazdak Momen <mazdak.momen@gmail.com>
2015-01-06 18:43:15 -0800

    Changed questionsTemplate to live in html for easier reading and modifying

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,28 +11,16 @@ $(function() {
   });
 });
 
-var questionsTemplate = _.template(
-  "<ol>" +
-    "<% _.forEach(content, function(data) { %>" +
-      "<li><%= data.question %> </li>" +
-        "<ul class=\"choices-block\">" +
-          "<% var options = data.choices.split(';') %>" +
-          "<% options.forEach(function (o) { %>" +
-            "<li><input type='radio' name='question-<%= data.id %>' value='<%= o %>'><%= o %></li>" +
-          "<% }) %>" +
-        "</ul>" +
-    "<% }) %>" +
-  "</ol>"
-);
-
 $(document).on('click', '.quiz', function() {
+  var source = $('.question-template').html();
+  var questionsTemplate = _.template(source);
   $('#quizzes-display').empty(); //clear the question block 
   var quizID = $(this).data("id");
   var url = "/quizzes/" + quizID + "/questions";
   $.get(url, function(questionData) {
     console.log(questionData);
-    var $el = questionsTemplate({content: questionData});
+    var compiledQuestionsTemplate = questionsTemplate({content: questionData});
+    var $el = $(compiledQuestionsTemplate);
     $('#quizzes-display').append($el);
   });
 });
-
